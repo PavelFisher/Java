80aa83e142f54894331b280e7d93776dcdc1ebf0
PavelFisher <125215270+PavelFisher@users.noreply.github.com>
2025-01-26 12:00:04 +0300

    Time: 33 ms (5.64%) | Memory: 45.6 MB (5.49%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,21 +1,14 @@
 class Solution {
         public boolean isValid(String s) {
-            char[][] brackets = {{'(', ')'}, {'{', '}'}, {'[', ']'}};
             String subS = s.trim();
             if (s.length() % 2 > 0) return false;
             while (subS.length() > 0) {
-                boolean check = false;
-                for (int i = 0; i < subS.length() - 1; i++) {
-                    if (subS.charAt(i) == brackets[0][0] && subS.charAt(i + 1) == brackets[0][1] ||
-                            subS.charAt(i) == brackets[1][0] && subS.charAt(i + 1) == brackets[1][1] ||
-                            subS.charAt(i) == brackets[2][0] && subS.charAt(i + 1) == brackets[2][1]) {
-                        if (subS.length() == 2) return true;
-                        subS = subS.substring(0, i) + subS.substring(i + 2, subS.length());
-                        check = true;
-                    }
-                }
-                if (!check) return false;
+                int checkLength = subS.length();
+                subS = subS.replace("()", "");
+                subS = subS.replace("{}", "");
+                subS = subS.replace("[]", "");
+                if (subS.length() == checkLength) return false;
             }
-            return false;
+            return true;
         }
 }
