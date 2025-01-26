de74222d3d06fcada959357fd791de34ba7e9154
PavelFisher <125215270+PavelFisher@users.noreply.github.com>
2025-01-26 12:30:17 +0300

    Time: 3 ms (41.94%) | Memory: 42 MB (34.31%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,14 +1,33 @@
 class Solution {
         public boolean isValid(String s) {
-            String subS = s.trim();
-            if (s.length() % 2 > 0) return false;
-            while (subS.length() > 0) {
-                int checkLength = subS.length();
-                subS = subS.replace("()", "");
-                subS = subS.replace("{}", "");
-                subS = subS.replace("[]", "");
-                if (subS.length() == checkLength) return false;
+            Stack<Character> stack = new Stack<>();
+            Map<Character, Character> mapping = new HashMap<>();
+            mapping.put(')', '(');
+            mapping.put('}', '{');
+            mapping.put(']', '[');
+
+            for (char c : s.toCharArray()) {
+                if (mapping.containsValue(c)) {
+                    stack.push(c);
+                } else if (mapping.containsKey(c)) {
+                    if (stack.isEmpty() || mapping.get(c) != stack.pop()) {
+                        return false;
+                    }
+                }
             }
-            return true;
+
+            return stack.isEmpty();
         }
+        // public boolean isValid(String s) {
+        //     String subS = s.trim();
+        //     if (s.length() % 2 > 0) return false;
+        //     while (subS.length() > 0) {
+        //         int checkLength = subS.length();
+        //         subS = subS.replace("()", "");
+        //         subS = subS.replace("{}", "");
+        //         subS = subS.replace("[]", "");
+        //         if (subS.length() == checkLength) return false;
+        //     }
+        //     return true;
+        // }
 }
